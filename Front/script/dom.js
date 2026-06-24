5cd9bfb5118c86ad695d8745afc7cc38b7755936
sbalbin1 <aborda@pioix.edu.ar>
2026-06-24 10:02:52 -0300

    Login FINISHED
    Se termino el login leo, ya esta el LOKITO local. Y se arreglo el login :D

--- Front/script/dom.js
+++ Front/script/dom.js
@@ -17,7 +17,7 @@ function agregarImagenes(jug1,jug2){
     document.getElementById("img_player2").src = jug2[0].url_foto;
     document.getElementById("name2").innerHTML = jug2[0].nombre;
     document.getElementById("stat2").innerHTML = jug2[1];
-
+}
 
 
 function add_nombre(){
